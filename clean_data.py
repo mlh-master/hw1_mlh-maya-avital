c3f2b3be3b94f754b2323a97438dd7a8faa56386
Avital\Avital Epstein <avitalep@campus.technion.ac.il>
2020-12-08 20:29:35 +0200

    My first commit

--- clean_data.py
+++ clean_data.py
@@ -17,10 +17,24 @@ def rm_ext_and_nan(CTG_features, extra_feature):
     :return: A dictionary of clean CTG called c_ctg
     """
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
+   
+    c_ctg = CTG_features.drop([extra_feature], axis=1)
+    is_num = c_ctg.applymap(lambda x: isinstance(x, (int, float)))
+    c_ctg1 = c_ctg.where(is_num, other=np.nan, inplace=False, axis=None, level=None, errors='raise', try_cast=False)
+    #print(c_ctg1)
+    c_ctg3=dict()
+    for key in c_ctg1.keys():
+        c_ctg2=c_ctg1[key].loc[:].dropna().values.astype(float)
+        c_ctg3[key]=c_ctg2
 
     # --------------------------------------------------------------------------
-    return c_ctg
+    return c_ctg3
 
+def rand_sampling(x, var_hist):
+    if np.isnan(x):
+        rand_idx = np.random.choice(len(var_hist))
+        x = var_hist[rand_idx][0]
+    return x
 
 def nan2num_samp(CTG_features, extra_feature):
     """
@@ -29,23 +43,30 @@ def nan2num_samp(CTG_features, extra_feature):
     :param extra_feature: A feature to be removed
     :return: A pandas dataframe of the dictionary c_cdf containing the "clean" features
     """
-    c_cdf = {}
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
-
-    # -------------------------------------------------------------------------
-    return pd.DataFrame(c_cdf)
-
+    c_ctg = CTG_features.drop([extra_feature], axis=1)
+    is_num = c_ctg.applymap(lambda x: isinstance(x, (int, float)))
+    c_ctg1 = c_ctg.where(is_num, other=np.nan, inplace=False, axis=None, level=None, errors='raise', try_cast=False)
+    c_cdf = pd.DataFrame()
+    for key in c_ctg1.keys():
+        feat=[key]
+        c_ctg2=c_ctg1[feat].loc[:].dropna().values.astype(float) #no Nan's
+        new = c_ctg1[feat].applymap(lambda x: rand_sampling(x, c_ctg2))
+        new1 = pd.DataFrame.from_dict(new)
+        c_cdf[feat] = new1  
+    return c_cdf
 
 def sum_stat(c_feat):
-    """
+    stat_df = pd.DataFrame()
+    stat_df = stat_df.append(c_feat.min().transpose(), ignore_index = True)
+    stat_df = stat_df.append(c_feat.quantile(0.25, axis = 0).transpose(), ignore_index = True)
+    stat_df = stat_df.append(c_feat.quantile(0.5, axis = 0).transpose(), ignore_index = True)
+    stat_df = stat_df.append(c_feat.quantile(0.75, axis = 0).transpose(), ignore_index = True)
+    stat_df = stat_df.append(c_feat.max().transpose(), ignore_index = True)
+    stat_df.index = ['min', 'Q1', 'med', 'Q3', 'max']
+    d = stat_df.to_dict()
 
-    :param c_feat: Output of nan2num_cdf
-    :return: Summary statistics as a dicionary of dictionaries (called d_summary) as explained in the notebook
-    """
-    # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
-
-    # -------------------------------------------------------------------------
-    return d_summary
+    return d
 
 
 def rm_outlier(c_feat, d_summary):
@@ -57,9 +78,17 @@ def rm_outlier(c_feat, d_summary):
     """
     c_no_outlier = {}
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
+    for key in c_feat:
+        out_h = d_summary[key]['Q3'] + 1.5*(d_summary[key]['Q3']-d_summary[key]['Q1'])
+        out_l = d_summary[key]['Q3'] - 1.5*(d_summary[key]['Q3']-d_summary[key]['Q1'])
+        c_new = c_feat[key]
 
+        new = (c_feat[key].loc[:]<out_h) & (c_feat[key].loc[:]>out_l)
+        c_no_outlier[key] = c_new.where(new, other=np.nan, inplace=False, axis=None, level=None, errors='raise', try_cast=False)
     # -------------------------------------------------------------------------
     return pd.DataFrame(c_no_outlier)
+
+
 
 
 def phys_prior(c_cdf, feature, thresh):
@@ -71,7 +100,11 @@ def phys_prior(c_cdf, feature, thresh):
     :return: An array of the "filtered" feature called filt_feature
     """
     # ------------------ IMPLEMENT YOUR CODE HERE:-----------------------------
-
+   
+    filt_feature = []
+    new = (c_cdf[feature].loc[:]<thresh) 
+    filt_feature_1 = c_cdf[feature].where(new, other=np.nan, inplace=False, axis=None, level=None, errors='raise', try_cast=False)
+    filt_feature=filt_feature_1.loc[:].dropna().values.astype(float) #no Nan's
     # -------------------------------------------------------------------------
     return filt_feature
 
@@ -87,6 +120,42 @@ def norm_standard(CTG_features, selected_feat=('LB', 'ASTV'), mode='none', flag=
     """
     x, y = selected_feat
     # ------------------ IMPLEMENT YOUR CODE HERE:------------------------------
+    result = CTG_features.copy()
 
+    if mode == 'standard':
+            for feature_name in CTG_features.columns:
+                mean_value = CTG_features[feature_name].mean()
+                std_value = np.std(CTG_features[feature_name])
+                result[feature_name] = (CTG_features[feature_name] - mean_value) / std_value
+
+    if mode == 'MinMax':
+        for feature_name in CTG_features.columns:
+            max_value = CTG_features[feature_name].max()
+            min_value = CTG_features[feature_name].min()
+            result[feature_name] = (CTG_features[feature_name] - min_value) / (max_value - min_value)
+
+
+    if mode == 'mean':
+        for feature_name in CTG_features.columns:
+            max_value = CTG_features[feature_name].max()
+            min_value = CTG_features[feature_name].min()
+            mean_value = CTG_features[feature_name].mean()
+            result[feature_name] = (CTG_features[feature_name] - mean_value) / (max_value - min_value)
+
+
+
+    nsd_res = result
+    
+    if flag:
+        fig1 = plt.figure()
+        ax1 = fig1.add_subplot(1,1,1)
+            #n, bins, patches = ax1.hist(result[x], bins = 50)
+        ax1.hist(result[x], bins = 50)
+        ax1.title.set_text(x)
+        fig2 = plt.figure()
+        ax2 = fig2.add_subplot(1,1,1)
+        #n, bins, patches = ax2.hist(result[y], bins = 50)
+        ax2.hist(result[y], bins = 50)
+        ax2.title.set_text(y)
     # -------------------------------------------------------------------------
     return pd.DataFrame(nsd_res)
